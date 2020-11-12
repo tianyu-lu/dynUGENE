d9733294bd345bbcf6be06c4881e24b794f6657e
tianyu-lu <laurenicious@gmail.com>
2020-11-11 19:14:54 -0500

    plotTrajectory() done

---
+++ R/plotTrajectory.R
@@ -0,0 +1,48 @@
+#' Plots individual node dynamics
+#'
+#' Given the result of simulate() (an object of class "simulation"), plots the
+#' dynamics of the specified nodes over time. Specify nodes by their names.
+#'
+#' @param simulation The result of simulate().
+#' @param node.names The names of the nodes to plot. Minimum one name.
+#'  Maximum six names, as the plot would get cluttered beyond this number.
+#'
+#' @examples
+#' \dontrun{
+#'   x0 <- Repressilator[1, 2:7]
+#'   ugene <- inferNetwork(Repressilator)
+#'   simulation <- simulate(ugene, x0)
+#'   plotTrajectory(simulation, c("p3", "p2", "p1"))
+#' }
+
+plotTrajectory <- function(simulation, node.names) {
+  cols <- c('#1B9E77', '#D95F02', '#7570B3',
+            '#E7298A', '#66A61E', '#E6AB02')
+  gene.names <- colnames(traj.x)
+
+  if (! all(node.names %in% gene.names)) {
+    stop("One or more node names do not exist as a gene name.")
+  }
+  if (length(node.names) < 1) {
+    stop("Must provide at least one node name to plot.")
+  }
+  if (length(node.names) > 6) {
+    stop("Currently does not support plotting more than 6 nodes simultaneously.")
+  }
+
+  plot(traj.t, traj.x[node.names[1]], type = 'l', xlab = "Time",
+       ylab = "Expression/\nConcentration", col=cols[1])
+  if (length(node.names) > 1) {
+    for (i in 2:length(node.names)) {
+      points(traj.t, traj.x[node.names[i]], type = 'l',
+             xlab = "Time", col=cols[i])
+    }
+  }
+
+  legend("topleft",
+         legend = node.names,
+         pch=15,
+         col = cols[1:length(node.names)],
+         bty = "n")
+  title("Inferred System Dynamics")
+}
--- R/simulate.R
+++ R/simulate.R
@@ -14,13 +14,13 @@
 #' of random forests are treated as deterministic (FALSE) or as a distribution
 #' from which a sample is drawn (TRUE). Defaults to FALSE.
 #'
-#' @return Returns an object of class "trajectory" containing the time stamps
+#' @return Returns an object of class "simulation" containing the time stamps
 #' in result.t and simulated values of all genes in result.x
 #'
 #' @examples
 #' \dontrun{
 #'   x0 <- Repressilator[1, 2:7]
-#'   ugene <- inferNetwork(data)
+#'   ugene <- inferNetwork(Repressilator)
 #'   simulate(ugene, x0)
 #' }
 #'
@@ -46,26 +46,28 @@ simulate <- function(ugene, x0, tend=100, dt=0.1, stochastic=FALSE) {
 
   # Simulation loop
   xt0 <- x0
-  traj.t <- seq(1, tend, by=dt)
-  traj.x <- matrix(data=0, nrow=length(traj.t), ncol=ngenes)
+  traj.t <- seq(0, tend, by=dt)
+  traj.x <- data.frame(matrix(nrow=length(traj.t), ncol=ngenes))
+  colnames(traj.x) <- colnames(ugene$network)
   traj.x[1, ] <- xt0
-  tstep <- 2
-  for (t in traj.t) {
-    curr.xt <- vector(mode="numeric", length=ngenes)
+  for (t in 2:length(traj.t)) {
+    curr.xt <- data.frame(matrix(nrow=1, ncol=ngenes))
     for (i in 1:ngenes) {
       thisrf <- ugene$model[[i]]
       y <- predict(thisrf, xt0)  # y = (xt1 - xt0)/(t1 - t0) + alpha*xt0
 
-      curr.xt[i] <- (y - ugene$alpha[i]*xt0[i])*dt + xt0[i]
+      curr.xt[i] <- (y - ugene$alpha[i]*as.numeric(xt0[i]))*dt + as.numeric(xt0[i])
     }
-    traj.x[tstep, ] <- curr.xt
-    tstep <- tstep + 1
+    dim(curr.xt) <- c(1, ngenes)
+    traj.x[t, ] <- curr.xt
+    xt0 <- curr.xt
   }
 
   results <- list(t=traj.t, x=traj.x)
-  class(results) <- "trajectory"
+  class(results) <- "simulation"
   return(results)
 }
 
 #[END]
+
 
